ec2e239330cae34ce425c287684d6fcf196c6242
kolomiec <kolomiecsergeyk@gmail.com>
2013-11-25 22:15:20 +0200

    Parse form data

--- src/requestHandlers.js
+++ src/requestHandlers.js
@@ -1,5 +1,6 @@
-var querystring = require("querystring"),
-    fs = require("fs");
+var querystring = require("querystring");
+var fs = require("fs");
+var util=require('util');
 
 
 function getHTML(viewName, response, request) {
@@ -19,25 +20,23 @@ function index(response, request) {
 }
 
 function createPost(response, request) {
-    console.log("Request handler 'upload' was called.");
+    var postData = "";
 
-    var form = new formidable.IncomingForm();
-    console.log("about to parse");
-    form.parse(request, function(error, fields, files) {
-        console.log("parsing done");
-
-        /* Возможна ошибка в Windows: попытка переименования уже существующего файла */
-        fs.rename(files.upload.path, "/tmp/test.png", function(err) {
-            if (err) {
-                fs.unlink("/tmp/test.png");
-                fs.rename(files.upload.path, "/tmp/test.png");
-            }
-        });
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write("received image:<br/>");
-        response.write("<img src='/show' />");
-        response.end();
+    request.addListener("data", function(postDataChunk) {
+        postData += postDataChunk;
+        console.log("Received POST data chunk '"+
+            postDataChunk + "'.");
     });
+    request.on('end', function () {
+        util.inspect(querystring.parse(postData));
+    });
+
+    response.writeHead(302, {
+        'Location': '/index'
+        //add other headers here...
+    });
+    response.end();
+
 }
 
 function showPost(response) {
